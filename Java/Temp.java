ab0de4ab6ad3f6678f466f039b97aff5aa8db3de
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-06 12:53:48 +0530

    Create Temp.java

---
+++ Java/Temp.java
@@ -0,0 +1,12 @@
+
+import java.lang.*; //after jdk 7 it is not necessary to include
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		System.out.println("Hello World");
+		
+	}
+}
