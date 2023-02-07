1b8291fd0054c2584cb22f1aec34a48cd71a3b3c
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-07 12:33:00 +0530

    Update Temp.java

--- Java/Temp.java
+++ Java/Temp.java
@@ -1,11 +1,25 @@
+/*
+IDE=Syntax analyser+compiler+run (Integrated development Environment)
+JRE=java runtime environment (Provide all facility to run java program easily)
 
-import java.lang.*; //after jdk 7 it is not necessary to include
+1. JDK (Java development kit, it is plateform dependent, it will run automatically)
+2. Compiler (JDK as well as )
+3. JVM (java virtual machine, it is plateform dependent but it makes the java program to platefom independent)
+
+
+java program run krne k baad ek .class file generate hogi that will be PLATEFORM INDEPENDENT
+Thus java is PLATEFORM INDEPENDENT
+*/
+
+
+import java.lang.*;  //after jdk 7 it is not necessary to include
 
 
 public class Main
 {
+	//int x=0; Class level variable
 	public static void main(String[] args) {
-	    
+	    //int x=0; Local level variable
 		System.out.println("Hello World");
 		
 	}
