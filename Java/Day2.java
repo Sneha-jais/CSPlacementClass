43f8af019138c62117884940ec2e32c060f14df7
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-07 12:56:11 +0530

    Create Day2.java

---
+++ Java/Day2.java
@@ -0,0 +1,38 @@
+/*
+Facultu Chart:
+
+M represnt no. of male faculty
+F represnt no. of female faculty
+
+
+a.)
+1. Each room will have two inviligator
+2. try to have combination of 1 M and 1 F faculty in each room
+3. no combination should be repeated
+4. total chart will be of 5 days
+
+b.)Faculty M5, F7 are proffesor and their duties can't be exceed more than two days
+
+*/
+
+import java.utill.*;
+
+public class Main
+{
+    //int facultyCharge ---> it is attribute because it is defined inside class not inside method
+    
+	public static void main(String[] args) {
+	
+	int M=0,F=0,R=0;
+	Scanner input=new Scanner(System.in); // in new Scanner, Scanner is a special method that is being called CONSTRUCTOR
+	M=input.nextInt();   //nextLong,nextChar, for string-- next and for string with spaces nextLine;
+	F=input.nextInt();
+	R=input.next();
+	
+	//M,F,R are not attributes they are variable
+	
+	
+	
+	
+	}
+}
