a1c4f89b7c9996128462562dc6be7bf8371ed654
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-08 12:50:55 +0530

    Create Day3.java

---
+++ Java/Day3.java
@@ -0,0 +1,10 @@
+
+/*
+structure of class is like class of java... the main difference is that in java there is method.
+Java is case sensitive language
+
+java is a stirctly typed while python is loosely typed language... i.e. in python we don't need to define the data type of variables.
+
+
+
+*/
