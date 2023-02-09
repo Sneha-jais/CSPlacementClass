719c361ee0b5e471361de017851ba0148e745cf2
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-09 12:26:29 +0530

    Update Day3.java

--- Java/Day3.java
+++ Java/Day3.java
@@ -8,3 +8,35 @@ java is a stirctly typed while python is loosely typed language... i.e. in pytho
 
 
 */
+
+
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+    String name;
+   int rollNo;
+    int DAA_marks;
+    int coa;
+    
+	public static void main(String[] args) {
+	
+	Main st1=new Main(); //new because it will allocate memory 
+	st1.name="Sneha";
+	st1.rollNo=162;
+	Main st2=new Main(); //new because it will allocate memory 
+	st2.name="Jaiswal";
+	st2.rollNo=27;
+	
+	System.out.println("Sudent 1 name is "+ st1.name);
+	
+		
+	}
+}
+
