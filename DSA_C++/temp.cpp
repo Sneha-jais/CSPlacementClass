500a49ccf7922baf2eb51c4e3dc2aadcd319c1b3
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2023-02-13 16:02:39 +0530

    Update temp.cpp

--- DSA_C++/temp.cpp
+++ DSA_C++/temp.cpp
@@ -1,2 +1,10 @@
 /* M3
 */
+//Push(),pop(),top...
+
+
+push_bottom(); // take another stack,push all elements to new stack, now push the element to old stack and again push all elements to old stack
+
+
+
+
